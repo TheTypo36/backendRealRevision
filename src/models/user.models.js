16a9f90d3496978e46214052dcacaa2bc7e38dbd
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-02-03 23:37:06 +0530

    add user and video model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,14 +1,48 @@
 import mongoose from "mongoose";
-
+import Jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    username: {
       type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //optimised the searching field
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
       required: true,
     },
-    subscribers: {
+    coverImage: {
+      type: String, //cloudinary at
+    },
+    password: {
       type: String,
-      required: true,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
     },
   },
   {
@@ -16,4 +50,41 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (req, res, next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function () {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  Jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefershToken = function () {
+  Jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFERESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFERESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", userSchema);
