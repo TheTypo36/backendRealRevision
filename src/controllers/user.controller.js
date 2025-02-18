5e5e7b5154bed7d0544eadf3701bb79915cec74b
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-02-18 12:03:06 +0530

    added subscription model and resolve a jwt bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { jwt } from "jsonwebtoken";
+import  jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
