d1c44cde714d4d9ebc17fa11c40e3348da7e33a2
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-02-27 16:11:58 +0530

    routes for new controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middlemare.js";
@@ -26,5 +33,18 @@ router.route("/login").post(loginUser);
 //these routes are only given when user is loggedIn
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/curr-user").get(verifyJwt, getCurrUser);
+//dont' use post, user patch because post update all the details whereas patch
+// only update what you have changed
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
 
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateCoverImage);
+
+//params is diffcult  part
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 export default router;
